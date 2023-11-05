04b051b131b8c6a8bd420d288dc8f51d63c2cf11
xiaomi-dew <786757593@qq.com>
2023-11-05 23:18:15 +0800

    视频弹幕

--- 原型/2.js
+++ 原型/2.js
@@ -2,23 +2,25 @@ Car.prototype.name = 'BMW'
 Car.prototype.lang = 4900
 Car.prototype.height = 1400
 
+//构造函数
 function Car(owner,color){
-    // this.name = 'BMW'
-    // this.lang = 4900
-    // this.height = 1400
+    this.name = 'BMW'
+    this.lang = 4900
+    this.height = 1400
     this.owner = owner
     this.color = color
 }
+//创建实例对象
+var car = new Car('BMW','red')
+var car2 = new Car('宝马','black')
 
-var car = new Car('小米','red')
-var car2 = new Car('小陈','black')
-
-
+console.log(car);
+console.log(car2);
 //Car.prototype.name = '红旗'
 // delete car.name
-delete Car.prototype.name
+// delete Car.prototype.name
 
-car2.name = '红旗'
-console.log(car2.name);
-console.log(car2);
-console.log(car.name);
+// car2.name = '红旗'
+// console.log(car2.name);
+// console.log(car2);
+// console.log(car.name);
