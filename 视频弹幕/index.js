615b030844d8d2c18def6bba40316ef9ea05d9f0
xiaomi-dew <786757593@qq.com>
2023-11-15 11:02:40 +0800

    弹幕完成

--- 视频弹幕/index.js
+++ 视频弹幕/index.js
@@ -1,36 +1,48 @@
 //模拟历史弹幕数据
 const data = [
-    { value: '今天你掘金了吗？', color: 'red', fontSize: 22, time: 5 },
-    { value: '不听不听，就要恋爱', color: 'green', fontSize: 30, time: 10 },
-    { value: '小米露我男神', color: '#00a1f5', fontSize: 26, time: 13 },
-    { value: '掘金掘金，每日掘金', color: '#00a1f5', fontSize: 26, time: 13 },
-    { value: '掘金文章每日来一篇', color: '#00a1f5', fontSize: 26, time: 13 },
+    { text: '今天你掘金了吗？', color: 'red', fontSize: 22, time: 5 },
+    { text: '不听不听，就要恋爱', color: 'green', fontSize: 30, time: 10 },
+    { text: '小米露我男神', color: 'red', fontSize: 35, time: 13 },
+    { text: '掘金掘金，每日掘金', color: 'pink', fontSize: 50, time: 13 },
+    { text: '掘金文章每日来一篇', color: '#00a1f5', fontSize: 40, time: 13 },
   ]
+
+  barrageManage.prototype.render = function(){
+    //清除画布
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+    //将弹幕绘制在画布上
+    this.barrageRender()  //把所有弹幕绘制在画布上
+    if(!this.isPaused){ //视频是播放状态
+      requestAnimationFrame(this.render.bind(this))
+    }
+  }
+
 
 barrageManage.prototype.barrageRender = function(){
   //拿到视频当前播放的时间
   const time = this.video.currentTime
   //实现每一条弹幕左移
   this.allBarrage.forEach(function(barrage) {
-    if(time >= barrage.time){
+    if(time >= barrage.time && !barrage.flag){
+      if(!barrage.isInit){
+        barrage.init()
+        barrage.isInit = true
+      }
       barrage.x -= barrage.speed
       barrage.renderEach()
+
+      //弹幕从左边出去了
+      if(barrage.x <= -barrage.width){
+        barrage.flag = true
+      }
     }
   });
 }
 
-barrageManage.prototype.render = function(){
-  //清除画布
-  this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
-  //将弹幕绘制在画布上
-  this.barrageRender()
-  if(!this.isPaused){ //视频是播放状态
-    requestAnimationFrame(this.render.bind(this))
-  }
-}
 barrageManage.prototype.add = function(obj){
   this.allBarrage.push(new Barrage(obj,this))
 }
+
 //整理弹幕
 function barrageManage(canvas,video,sendData={}) {
     if(!canvas || !video) return
@@ -60,41 +72,52 @@ function barrageManage(canvas,video,sendData={}) {
     this.isPaused = true //播放开关
 
     //获取到所有最终被修饰好的弹幕
-    this.allBarrage = this.data.map((item)=>{new Barrage(item,this)})
+    this.allBarrage = this.data.map((item)=> new Barrage(item,this) )
 
     //实现弹幕移动效果
     this.render() 
 }
 
-//创建一个整理弹幕的实例对象
-const barragemanage = new barrageManage(canvas,video,{data})
-video.addEventListener('play',()=>{
-  barragemanage.isPaused = false //将播放状态开关设置为播放
-
-})
-
 //弹幕初始化
 Barrage.prototype.init = function(){
-  this.color = 
+  this.color = this.obj.color || this.context.color
+  this.fontSize = this.obj.fontSize || this.context.fontSize
+  this.speed = this.context.speed
+  this.opacity = this.context.opacity 
+
+  //获取弹幕的宽度
+  const p = document.createElement('p')
+  p.style.fontSize = this.fontSize + 'px'
+  p.innerHTML = this.text
+  document.body.appendChild(p)
+  this.width = p.offsetWidth
+  document.body.removeChild(p)
+
+  //设置弹幕的初始位置
+  this.x = this.context.canvas.width
+  this.y = this.context.canvas.height*Math.random()
+  //弹幕超出画布上边界
+  if(this.y < this.fontSize){
+    this.y = this.fontSize
+  }
+  //弹幕超出画布下边界
+  if(this.y > this.context.canvas.height - this.fontSize){
+    this.y = this.context.canvas.height - this.fontSize
+  }
 }
+
+Barrage.prototype.renderEach = function(){
+  this.context.ctx.font = `${this.fontSize}px Arial`
+  this.context.ctx.fillStyle = this.color
+  this.context.ctx.fillText(this.text, this.x,this.y)
+}
+
 //修饰每一条弹幕
 function Barrage(obj,context){ //context == this
-  this.color = obj.color || 
-}
-
-
-
-
-function send() { 
-  //获取到用户填的弹幕信息
-  const time = video.currentTime
-  const text = $text.value
-  const color = $color.value
-  const fontSize = $range.value
-  const obj = {time, text, color, fontSize}
-
-  //添加弹幕
-  barragemanage.add(obj)
+  this.text = obj.text
+  this.time = obj.time
+  this.obj = obj
+  this.context = context //挂载上文this
 }
 
 const canvas = document.getElementById('canvas')
@@ -103,6 +126,25 @@ const $text = document.getElementById('text')
 const btn = document.getElementById('btn')
 const $color = document.getElementById('color')
 const $range = document.getElementById('range')
+
+  //创建一个整理弹幕的实例对象
+  const barragemanage = new barrageManage(canvas,video,{data})
+  video.addEventListener('play',()=>{
+    barragemanage.isPaused = false //将播放状态开关设置为播放
+    barragemanage.render()  //处理每一条弹幕
+  })
+
+  function send() { 
+    //获取到用户填的弹幕信息
+    const time = video.currentTime
+    const text = $text.value
+    const color = $color.value
+    const fontSize = $range.value
+    const obj = {time, text, color, fontSize}
+  
+    //添加弹幕
+    barragemanage.add(obj)
+  }
 
 btn.addEventListener('click',send)
 $text.addEventListener('keyup', function(e) {
