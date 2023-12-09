ff4067d7e9794d43532ec547f55dc9facbe197d2
xiaomi-dew <786757593@qq.com>
2023-12-09 20:25:35 +0800

    vue

---
+++ vue/v3-music/src/components/common/IconPark.vue
@@ -0,0 +1,12 @@
+<template>
+    <Component :is="icon" :theme="theme" :size="size" :spin="spin" :fill="fill" :strokeLinecap="strokeLinecap"
+               :strokeLinejoin="strokeLinejoin" :strokeWidth="strokeWidth"/>
+</template>
+  
+  <script setup >
+//   定义父组件传过来的参数
+//   vue组件的一种声明方式，本来是有各种图标，Component + is=""入侵式的组件声明方式
+  import { defineProps } from 'vue';
+// 子组件使用父组件传过来的props先声明才能有，为了安全
+  defineProps(['icon', 'theme', 'size', 'spin', 'fill', 'strokeLinecap', 'strokeLinejoin', 'strokeWidth'])
+  </script>
--- vue/v3-music/src/components/layout/menu/MenuList.vue
+++ vue/v3-music/src/components/layout/menu/MenuList.vue
@@ -1,5 +1,6 @@
 <template>
-    <div>
+    <!-- 用户体验的直觉，滚动的列表 -->
+    <ElScrollbar>
         <div class="mt-10 pl-6 pr-6 space-y-1.5" 
         v-for="menuItem in menus" :key="menuItem.menus.keys">
             <div class="menu-title text-main">{{menuItem.name}}</div>
@@ -10,88 +11,29 @@
                 :class="{'active':currentKey === menu.key}"
                 @click="changeMenu(menu)"
             >
+                <IconPark :icon="menu.icon" size="18" :theme="menu.theme"/>
                 <span class="ml-1">{{ menu.name }}</span>
             </div>
         </div>
-    </div>
+    </ElScrollbar>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import {Planet, Music, VideoOne, Fm, Like, Computer, DownloadThree, PlayTwo} from '@icon-park/vue-next'
-let currentKey = ref('discover')
-const changeMenu = (menu)=>{
-    currentKey.value = menu.key
-}
-const menus = [
-        {
-            name: "在线音乐",
-            menus: [
-                {
-                    name: "推荐",
-                    key: "discover",
-                    icon: Planet,
-                    theme: 'outline',
-                },
-                {
-                    name: "音乐馆",
-                    key: "music",
-                    icon: Music,
-                    theme: 'outline',
-                },
-                {
-                    name: "视频",
-                    key: "video",
-                    icon: VideoOne,
-                    theme: 'outline',
-                },
-                {
-                    name: "电台",
-                    key: "dj",
-                    icon: Fm,
-                    theme: 'outline',
-                },
-            ]
-        },
-        {
-            name: "我的音乐",
-            menus: [
-                {
-                    name: "我喜欢",
-                    key: "love",
-                    icon: Like,
-                    theme: 'outline',
-                },
-                {
-                    name: "本地歌曲",
-                    key: "local",
-                    icon: Computer,
-                    theme: 'outline',
-                },
-                {
-                    name: "下载歌曲",
-                    key: "download",
-                    icon: DownloadThree,
-                    theme: 'outline',
-                },
-                {
-                    name: "最近播放",
-                    key: "recently",
-                    icon: PlayTwo,
-                    theme: 'outline'
-                },
-            ]
-        }
-    ]
+import IconPark from '@/components/common/IconPark.vue'
+// 模块化 hooks编程
+// 数据离开组件，组件很好读
+// 范式，use开头 函数式编程
+import { useMenu } from './useMenu.js'
+const { menus,currentKey,changeMenu } = useMenu()
 
 </script>
 
 <style scoped>
-.meun-title{
+.menu-title{
     @apply text-xs pl-4 pr-4 pb-2;
 }
 
-.meun-item{
+.menu-item{
     @apply text-sm pl-4 pr-4 pt-1.5 pb-1.5 rounded cursor-pointer transition-colors flex items-center;
 }
 
