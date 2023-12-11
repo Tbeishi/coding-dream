9d685f671bf6cd2836b63cb145a13a7fbb98982e
xiaomi-dew <786757593@qq.com>
2023-12-11 21:36:10 +0800

    vue项目

---
+++ vue项目/vue-sell/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <Header/>
+  <router-view/>
+</template>
+
+<script>
+import Header from '@/components/header/header.vue'
+export default {
+  components:{
+    Header
+  }
+}
+</script>
+
+<style lang="less">
+
+</style>
