e5245998b4ea7a9cc57382dcdd4c5f8752f70d5f
小米露 <786757593@qq.com>
2024-02-27 22:27:01 +0800

    77.js

--- 力扣刷题/面试经典150题/77.js
+++ 力扣刷题/面试经典150题/77.js
@@ -1,1 +1,27 @@
 // 77.js
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+var backtracking = function(res,path,StartIndex,n,k){
+    if(path.length === k){
+        res.push([...path])
+        return
+    }
+    for(let i = StartIndex; i <= n && path.length < k ; i++){
+        path.push(i)
+        backtracking(res,path,i + 1,n,k)
+        path.pop()
+    }
+}
+
+var combine = function(n, k) {
+    const res = []
+    const path = []
+    backtracking(res,path,1,n,k)
+    return res
+};
+
+const res = combine(4,2)
+console.log(res);
